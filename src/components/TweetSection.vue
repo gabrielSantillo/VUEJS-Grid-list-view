78b05c50f27597e45032e53a9a85c9dc338a95fb
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-09 21:30:19 -0600

    Finished the assignment

--- src/components/TweetSection.vue
+++ src/components/TweetSection.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <button>Change tweet</button>
+    <div >
+        <button>Change view</button>
         <div class="tweet_card">
             
         </div>
@@ -10,38 +10,54 @@
 <script>
     export default {
 
-        mounted () {
-            let tweet_card = document.querySelector(`.tweet_card`);
-            tweet_card.insertAdjacentHTML(`beforeend`,
-            `
-            <p>${this.data[0][`user`]}</p>
-            `)
+        methods: {
+            changeGrid() {
+                let tweet_card = document.querySelector(`.tweet_card`);
+                tweet_card[`style`][`display`] = `grid`
+                tweet_card[`style`][`grid-auto-flow`] = `column`
+            }
         },
 
-        methods: {
-            injectTweet() {
-
-                
+        mounted () {
+            let tweet_card = document.querySelector(`.tweet_card`);
+            for(let i = 0;i <  this.tweets_array.length; i++) {
+            tweet_card.insertAdjacentHTML(`beforeend`,
+            `
+            <div class="tweet_grid">
+            <p>${this.tweets_array[i][`user`]}</p>
+            <p>${this.tweets_array[i][`tweet`]}</p>
+            <p>${this.tweets_array[i][`date`]}</p>
+            </div>
+            `)
             }
+
+            let button = document.querySelector(`button`);
+            button.addEventListener(`click`, this.changeGrid)
         },
 
         data() {
-            return [{
-                tweets: [`01_tweet`, `02_tweet`, `03_tweet`],
+            return {
+
+            tweets_array:  [
+            {
                 user: `Gabriel`,
-                date_created: new Date()
+                tweet: `hello`,
+                date: new Date()
             },
+
             {
-                tweets: [`04_tweet`, `05_tweet`, `06_tweet`],
-                user: `Nadine`,
-                date_created: new Date()
+                user: `Alex`,
+                tweet: `hello`,
+                date: new Date()
             },
+
             {
-                tweets: [`07_tweet`, `08_tweet`, `09_tweet`],
-                user: `Dawn`,
-                date_created: new Date()
+                user: `Colleen`,
+                tweet: `hello`,
+                date: new Date()
             }
-            ]
+            ] 
+            }
         },
     }
 </script>
